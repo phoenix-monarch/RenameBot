c65ced7f0ec7985e1ed0259d8a04fb33827bf454
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-08 01:36:42 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -60,7 +60,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif data == "about":
         await query.message.edit_text(
-            text=script.ABOUT_TXT.format(client.from_user.mention),
+            text=script.ABOUT_TXT,
             reply_markup=InlineKeyboardMarkup([[           
                InlineKeyboardButton("🗑️ᴄʟᴏsᴇ🗑️", callback_data = "close"),
                InlineKeyboardButton("⌫ʙᴀᴄᴋ☽", callback_data = "start")
