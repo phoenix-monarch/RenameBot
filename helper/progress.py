26aca9db674f26ca7e4d3479dd65073140be3e0d
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-07 18:24:21 +0530

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -27,7 +27,8 @@ async def progress_for_pyrogram(
             ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n**Speed**: {2}/s\n**ETA**: {3}\n".format(
+        tmp = progress + scrpit.PRG_BAR.format( 
+            round(percentage, 2),
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
