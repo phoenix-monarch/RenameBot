002d0d1fef0171e08760a5b7b6cb9452cc12bbf0
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-07 15:39:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply,CallbackQuery)
 import humanize
 import os
 from helper.database import  insert 
@@ -14,7 +14,75 @@ async def start(client,message):
             caption=script.START_TXT.format(message.from_user.first_name),
             reply_to_message_id = message.message_id,  
 	    reply_markup=InlineKeyboardMarkup(
-            [[ InlineKeyboardButton("Support" ,url="https://t.me/Elsasupportgp") ]  ]))
+            [[ InlineKeyboardButton("DEVS ", callback_data='dev')                
+                ],[
+                InlineKeyboardButton('SUPPORT', url='https://t.me/Elsasupportgp'),
+                InlineKeyboardButton('DONATE', callback='donate')
+                ],[
+                InlineKeyboardButton('ABOUT', callback_data='about'),
+                InlineKeyboardButton('HELP', callback_data='help') ]  ]))
+
+
+@Client.on_callback_query()
+async def cb_handler(client, query: CallbackQuery):
+    data = query.data 
+    if data == "start":
+        await query.message.edit_text(
+            text=script.START_TXT.format(message.from_user.first_name),
+            reply_markup=InlineKeyboardMarkup( [[
+                InlineKeyboardButton("DEVS ", callback_data='dev')                
+                ],[
+                InlineKeyboardButton('SUPPORT', url='https://t.me/Elsasupportgp'),
+                InlineKeyboardButton('DONATE', callback='donate')
+                ],[
+                InlineKeyboardButton('ABOUT', callback_data='about'),
+                InlineKeyboardButton('HELP', callback_data='help')
+                ]]
+                )
+            )
+    elif data == "help":
+        await query.message.edit_text(
+            text=script.HELP_TXT,
+            reply_markup=InlineKeyboardMarkup( [[
+               InlineKeyboardButton("CLOSE", callback_data = "close"),
+               InlineKeyboardButton("BACK", callback_data = "start")
+               ]]
+            )
+        )
+    elif data == "donate":
+        await query.message.edit_text(
+            text=script.DONATE_TXT,
+            reply_markup=InlineKeyboardMarkup( [[
+               InlineKeyboardButton("CLOSE", callback_data = "close"),
+               InlineKeyboardButton("BACK", callback_data = "start")
+               ]]
+            )
+        )
+    elif data == "about":
+        await query.message.edit_text(
+            text=script.ABOUT_TXT, 
+            reply_markup=InlineKeyboardMarkup([[           
+               InlineKeyboardButton("CLOSE", callback_data = "close"),
+               InlineKeyboardButton("BACK", callback_data = "start")
+               ]]
+            )
+        )
+    elif data == "dev":
+        await query.message.edit_text(
+            text=script.DEV_TXT,
+            reply_markup=InlineKeyboardMarkup( [[               
+               InlineKeyboardButton("CLOSE", callback_data = "close"),
+               InlineKeyboardButton("BACK", callback_data = "start")
+               ]]
+            )
+        )
+    elif data == "close":
+        try:
+            await query.message.delete()
+            await query.message.reply_to_message.delete()
+        except:
+            await query.message.delete()
+
 
 @Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
 async def send_doc(client,message):
