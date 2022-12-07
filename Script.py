3e5c8e37d2d98848f1bd3bf726da434788db2744
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-07 11:22:24 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,1 +1,14 @@
 class script(object):
+
+    START_TXT = """Hello 👋 {} 
+☞ I'm A Telegram File & Video Rename Bot With Permanent Thumbnail Support.
+
+☞ Send Me Any Telegram File/Video! 
+
+☞ Send A Photo To Save As Permanent Thumbnail!
+
+☞ Select Your Desired/Required Option! 
+
+☞ Then Wait Till The Process Get Completed!
+
+☞ Maintained By : @cinemala_com1"""
