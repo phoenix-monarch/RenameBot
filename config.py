029e0c6e660a64992ef360608da4496ad948e6e5
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-08 23:00:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,3 +14,4 @@ class Config(object):
      DB_URL = os.environ.get("DB_URL", "")
      START_PIC = os.environ.get("START_PIC", "")
      PORT = os.environ.get("PORT", "8080")
+     FORCE_SUB = os.environ.get("FORCE_SUB", "")
