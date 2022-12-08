2a06b32bcae068e6b4cba11d5712f91b0b4171f1
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-08 22:58:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import logging
+import logging.config
 from pyrogram import Client
 import os
 
@@ -5,22 +7,48 @@ from config import Config
 
 from aiohttp import web
 from plugins import web_server
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 
-if __name__ == "__main__" :
-    plugins = dict(
-        root="plugins"
-    )
-    app = Client(
-        "renamer",
-        bot_token=Config.TOKEN,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins
-    )
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
+class Bot(Client):
 
-    app.run()
+    def __init__(self):
+        super().__init__(
+            name="renamer",
+            bot_token=Config.TOKEN,
+            api_id=Config.APP_ID,
+            api_hash=Config.API_HASH,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
+
+    async def start(self):
+       await super().start()
+       me = await self.get_me()
+       self.mention = me.mention
+       self.username = me.username 
+       self.force_channel = FORCE_SUB
+       if FORCE_SUB:
+         try:
+            link = await self.export_chat_invite_link(FORCE_SUB)                  
+            self.invitelink = link
+         except Exception as e:
+            logging.warning(e)
+            logging.warning("Make Sure Bot admin in force sub channel")             
+            self.force_channel = None
+       app = web.AppRunner(await web_server())
+       await app.setup()
+       bind_address = "0.0.0.0"
+       await web.TCPSite(app, bind_address, PORT).start()
+       logging.info(f"{me.first_name} Started 👿👿👿")
+      
+
+    async def stop(self, *args):
+      await super().stop()      
+      logging.info("Bot Stopped")
+        
+bot = Bot()
+bot.run()
