227ded590da166fd20775cacf3f9996bc10fb97c
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-08 10:51:54 +0530

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -24,8 +24,8 @@ async def progress_for_pyrogram(
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "{0}{1}".format(
-            ''.join(["🟩" for i in range(math.floor(percentage / 5))]),
-            ''.join(["⬜" for i in range(20 - math.floor(percentage / 5))]))
+            ''.join(["▒" for i in range(math.floor(percentage / 5))]),
+            ''.join(["█" for i in range(20 - math.floor(percentage / 5))]))
             
         tmp = progress + script.PRGS_BAR.format( 
             round(percentage, 2),
