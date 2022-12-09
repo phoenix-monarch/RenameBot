6de97b18718753435bbf74a39165dd40a1b45245
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-09 22:57:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ DB_NAME = os.environ.get("DB_NAME", "my")
 #Your Mongo DB URL Obtained From mongodb.com
 DB_URL = os.environ.get("DB_URL", "")
 
-START_PIC = (os.environ.get("START_PIC", "")).split()
+START_PIC = (os.environ.get("START_PIC", "https://telegra.ph/file/0e3738d56ff92e363a1d9.jpg https://telegra.ph/file/a52e97c3371d6197e7723.jpg https://telegra.ph/file/da8e1ab9f84ba60494451.jpg https://telegra.ph/file/cfa28e7e2fe19adc9688c.jpg https://telegra.ph/file/be339f05202e87a05515c.jpg https://telegra.ph/file/45c1e617e959edc7f3aee.jpg https://telegra.ph/file/f0e48c64e7ee4ee304407.jpg https://telegra.ph/file/ac16ec543b2fb84be97d2.jpg https://telegra.ph/file/4f1303bf89fd84843f172.jpg")).split()
 
 PORT = os.environ.get("PORT", "8080")
 
