420a9a98641259fb40d44030de04c1362c2f25f0
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-09 07:59:19 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -1,23 +1,24 @@
 from pyrogram import Client, filters
-from helper.database import find, delthumb, addthumb
+from helper.database import db
 from Script import script
-@Client.on_message(filters.private & filters.command(['viewthumb']))
-async def viewthumb(client,message):
-		thumb = find(int(message.chat.id))
-		if thumb :
-			await client.send_photo(message.chat.id,photo =f"{thumb}")
-		else:
-			await message.reply_text(script.EMPTY_CUST)
-	
-	
-@Client.on_message(filters.private & filters.command(['delthumb']))
-async def removethumb(client,message):
-	delthumb(int(message.chat.id))
-	await message.reply_text(script.CUST_THUM_DEL)
 
-@Client.on_message(filters.private & filters.photo)
-async def addthumbs(client,message):
-	file_id = str(message.photo.file_id)
-	addthumb(message.chat.id , file_id)
-	await message.reply_text(script.CUST_THUM_SAV)
+@Client.on_message(filters.private & filters.command(['viewthumb']))
+async def viewthumb(client, message):    
+    thumb = await db.get_thumbnail(message.from_user.id)
+    if thumb:
+       await client.send_photo(
+	   chat_id=message.chat.id, 
+	   photo=thumb)
+    else:
+        await message.reply_text(script.EMPTY_CUST) 
+		
+@Client.on_message(filters.private & filters.command(['delthumb']))
+async def removethumb(client, message):
+    await db.set_thumbnail(message.from_user.id, file_id=None)
+    await message.reply_text(script.CUST_THUM_DEL)
 	
+@Client.on_message(filters.private & filters.photo)
+async def addthumbs(client, message):
+    dlb = await message.reply_text("ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ...")
+    await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
+    await dlb.edit(script.CUST_THUM_SAV)
