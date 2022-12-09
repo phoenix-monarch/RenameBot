75f1676a27b536427eba92450276b68af1b8d4d2
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-09 09:02:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import os
+import re
 
-class Config(object):
+
      # get a token from @BotFather
      TOKEN = os.environ.get("TOKEN", "")
      # The Telegram API things
      APP_ID = int(os.environ.get("APP_ID", ""))
      API_HASH = os.environ.get("API_HASH", "")
      #Array to store users who are authorized to use the bot
-     ADMIN = os.environ.get("ADMIN", "754495556")
+     ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
      #Your Mongo DB Database Name
      DB_NAME = os.environ.get("DB_NAME", "my")
      #Your Mongo DB URL Obtained From mongodb.com
@@ -15,3 +16,4 @@ class Config(object):
      START_PIC = os.environ.get("START_PIC", "")
      PORT = os.environ.get("PORT", "8080")
      FORCE_SUB = os.environ.get("FORCE_SUB", "")
+     FLOOD = int(os.environ.get("FLOOD", "5"))
