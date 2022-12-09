6953fedd3e121abd0006e123df834dd56af50b58
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-09 08:58:28 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,14 +1,15 @@
 class script(object):
 
     START_TXT = """<b>Hᴇʟʟᴏ 👋 {}
- I'ᴍ A Sɪᴍᴘʟᴇ Tᴇʟᴇɢʀᴀᴍ Fɪʟᴇ & Vɪᴅᴇᴏ Rᴇɴᴀᴍᴇ Bᴏᴛ Wɪᴛʜ Pᴇʀᴍᴀɴᴇɴᴛ Tʜᴜᴍʙɴᴀɪʟ Sᴜᴘᴘᴏʀᴛ.</b>"""
+ I'ᴍ A Sɪᴍᴘʟᴇ Tᴇʟᴇɢʀᴀᴍ Fɪʟᴇ & Vɪᴅᴇᴏ Rᴇɴᴀᴍᴇ Bᴏᴛ Wɪᴛʜ ᴄᴜꜱᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ ᴀɴᴅ Pᴇʀᴍᴀɴᴇɴᴛ Tʜᴜᴍʙɴᴀɪʟ Sᴜᴘᴘᴏʀᴛ.</b>"""
 
 
     DEV_TXT = """<b>☬ ᴛʜᴀɴᴋs ᴛᴏ ᴅᴇᴠᴇʟᴏᴘᴇʀs ☬
 
 ➥ <a href=https://github.com/Devil-Botz>『Dᴇᴠɪʟ࿐Tɢ』</a>
 ➥ <a href=https://github.com/Joelkb>  Jᴏᴇʟ ᠰ TɢX</a>
-➥ <a href=https://github.com/oVo-HxBots>  oVo-HxBots</a></b>"""
+➥ <a href=https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT>  PYRO_BOTZ</a> ᴇxᴛʀᴀ ғᴇᴀᴛᴜʀᴇs ᴀᴅᴅᴇᴅ
+➥ <a href=https://github.com/oVo-HxBots>  oVo-HxBots</a> ʙᴀsᴇ ʀᴇᴘᴏ</b>"""
 
     ABOUT_TXT = """<b>🍃 ᴍʏ ɴᴀᴍᴇ : {}
 🍃 ᴅᴇᴠ : <a href=https://t.me/Aswin_pm_Bot>『Dᴇᴠɪʟ࿐Tɢ』</a> 
@@ -16,7 +17,7 @@ class script(object):
 🍃 ʟᴀɴɢᴜᴀɢᴇ : <a href=https://www.python.org>ᴘʏᴛʜᴏɴ 3</a>
 🍃 ᴅᴀᴛᴀʙᴀsᴇ : <a href=https://cloud.mongodb.com>ᴍᴏɴɢᴏ ᴅʙ</a>
 🍃 sᴇʀᴠᴇʀ : <a href=https://studio.mogenius.com>ᴍᴏɢᴇɴɪᴜs</a>
-🍃 ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ3.5.5 [ ʙᴇᴛᴀ ] </b>"""
+🍃 ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ3.6.5 [ ʙᴇᴛᴀ ] </b>"""
 
     
     DONATE_TXT = """<b><i> ഞാൻ നിങ്ങളുടെ കയ്യിൽനിന്നും sᴜᴘᴘᴏʀᴛ മാത്രമേ പ്രതീക്ഷിക്കുന്നുള്ളൂ. @cinemala_com1 എന്ന ചാനലിലുള്ള മൂവീ ഗ്രൂപ്പിൽ ᴊᴏɪɴ ചെയ്യ്ത് sᴜᴘᴘᴏʀᴛ ചെയ്യൂ.😊</i></b> """
@@ -28,9 +29,20 @@ class script(object):
 〄࿐ /delthumb <b>use this command and delete your old thumbnile.</b>
 〄࿐ /viewthumb <b>use this command view your current thumbnile.</b>
 
- <b><u>📝 HOW TO RENAME A FILE 📝</u></b>
+ <b><u><i>📝 HOW TO RENAME A FILE 📝</i></u></b>
 
-〄࿐ <b>send any file and click rename option and type new file name and send select [ document, video, audio ]👈 choice this</b>"""
+〄࿐ <b>send any file and click rename option and type new file name and send select [ document, video, audio ]👈 choice this</b>
+
+ <b><u><i>🔖 HOW TO SET CUSTOM CAPTION 🔖</i></u></b>
+〄࿐ /set_caption - set a custom caption
+〄࿐ /see_caption - see your custom caption
+〄࿐ /del_caption - delete custom caption
+Example:- /set_caption 📝 File Name: {filename}
+💾 Size: {filesize}
+⏰ Duration: {duration}
+
+ᴛʜᴀɴᴋꜱ ᴛᴏ ᴘʏʀᴏ ʙᴏᴛᴢ
+ᴄᴜꜱᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ,ꜰᴏʀᴄᴇ ꜱᴜʙ & ꜰʟᴏᴏᴅ ᴀᴅᴅᴇᴅ ꜰʀᴏᴍ ᴘʏʀᴏ ʀᴇɴᴀᴍᴇ ʙᴏᴛ.ᴘʏʀᴏ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴀᴅᴅᴇᴅɪɴ ᴅᴇᴠꜱ"""
 
 
     PRGS_BAR = """\n <b>
@@ -58,3 +70,12 @@ class script(object):
     PLZ_ETR_NFIL = """𝐏𝐥𝐞𝐚𝐬𝐞 𝐞𝐧𝐭𝐞𝐫 𝐭𝐡𝐞 𝐧𝐞𝐰 𝐟𝐢𝐥𝐞𝐧𝐚𝐦𝐞..."""
 
     FSUB_MSG = """𝐒𝐎𝐑𝐑𝐘 𝐁𝐑𝐎 𝐘𝐎𝐔 𝐀𝐑𝐄 𝐍𝐎𝐓 𝐉𝐎𝐈𝐍𝐄𝐃 𝐌𝐘 𝐔𝐏𝐃𝐀𝐓𝐄 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 . 𝐏𝐋𝐄𝐀𝐒𝐄  𝐉𝐎𝐈𝐍 𝐀𝐍𝐃 𝐔𝐒𝐄 𝐓𝐇𝐈𝐒 𝐁𝐎𝐓"""
+
+    DEL_CAP = """𝙔𝙤𝙪𝙧 𝙘𝙖𝙥𝙩𝙞𝙤𝙣 𝙨𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮 𝙙𝙚𝙡𝙚𝙩𝙚𝙙"""
+
+    EMPTY_CAP = """𝙔𝙤𝙪 𝙙𝙤𝙣𝙩 𝙝𝙖𝙫𝙚 𝙖𝙣𝙮 𝙘𝙖𝙥𝙩𝙞𝙤𝙣"""
+
+    SAV_CAP = """𝗬𝗼𝘂𝗿 𝗰𝗮𝗽𝘁𝗶𝗼𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝘀𝗮𝘃𝗲𝗱"""
+
+    GIV_CAP = """𝙂𝙞𝙫𝙚 𝙢𝙚 𝙖 𝙘𝙖𝙥𝙩𝙞𝙤𝙣 𝙩𝙤 𝙨𝙚𝙩. 
+𝙀𝙭𝙖𝙢𝙥𝙡𝙚 :- /𝙨𝙚𝙩_𝙘𝙖𝙥𝙩𝙞𝙤𝙣: {filename} 𝙎𝙞𝙯𝙚: {filesize} 𝘿𝙪𝙧𝙖𝙩𝙞𝙤𝙣: {duration}"""
