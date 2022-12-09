bcf10ddcbda547042047d9408ca9a851a040ec15
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2022-12-09 07:52:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,34 +1,40 @@
+from asyncio import sleep
 from pyrogram import Client, filters
-from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply,CallbackQuery)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
+from pyrogram.errors import FloodWait
 import humanize
-import os
-from helper.database import  insert 
-from Script import script
-START_PIC = os.environ.get("START_PIC", "")
+import random
+from helper.txt import mr
+from helper.database import db
+from config import START_PIC, FLOOD, ADMIN 
+
 
 @Client.on_message(filters.private & filters.command(["start"]))
-async def start(client,message):
-	insert(int(message.chat.id))
-	await message.reply_photo(
-            photo=START_PIC,
-            caption=script.START_TXT.format(message.from_user.mention),
-            reply_to_message_id = message.message_id,  
-	    reply_markup=InlineKeyboardMarkup(
-            [[ InlineKeyboardButton("⚔ ᴅᴇᴠs ⚔", callback_data='dev')                
+async def start(client, message):
+    user = message.from_user
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id)             
+    txt=text=script.START_TXT.format(user.mention),
+    button=InlineKeyboardMarkup([[
+                InlineKeyboardButton("⚔ ᴅᴇᴠs ⚔", callback_data='dev')                
                 ],[
                 InlineKeyboardButton('〄 sᴜᴘᴘᴏʀᴛ 〄', url='https://t.me/Elsasupportgp'),
                 InlineKeyboardButton('〄 ᴅᴏɴᴀᴛᴇ 〄', callback_data='donate')
                 ],[
                 InlineKeyboardButton('〄 ᴀʙᴏᴜᴛ 〄', callback_data='about'),
-                InlineKeyboardButton('〄 ʜᴇʟᴘ 〄', callback_data='help') ]  ]))
-
+                InlineKeyboardButton('〄 ʜᴇʟᴘ 〄', callback_data='help')
+                ]])
+    if START_PIC:
+        await message.reply_photo(START_PIC, caption=txt, reply_markup=button)       
+    else:
+        await message.reply_text(text=txt, reply_markup=button, disable_web_page_preview=True)
 
 @Client.on_callback_query()
-async def cb_handler(client: Client, query: CallbackQuery):
+async def cb_handler(client, query: CallbackQuery):
     data = query.data 
     if data == "start":
         await query.message.edit_text(
-            text=script.START_TXT.format(query.from_user.mention),
+            text=text=script.START_TXT.format(query.from_user.mention),,
             reply_markup=InlineKeyboardMarkup( [[
                 InlineKeyboardButton("⚔ ᴅᴇᴠs ⚔", callback_data='dev')                
                 ],[
@@ -60,7 +66,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif data == "about":
         await query.message.edit_text(
-            text=script.ABOUT_TXT.format(query.message.from_user.mention),
+            text=script.ABOUT_TXT.format(client.mention),
+            disable_web_page_preview = True,
             reply_markup=InlineKeyboardMarkup([[    
                InlineKeyboardButton("〄 sᴏᴜʀᴄᴇ 〄", url="https://github.com/Devil-Botz/RenameBot") ],[      
                InlineKeyboardButton("🗑️ᴄʟᴏsᴇ🗑️", callback_data = "close"),
@@ -71,7 +78,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif data == "dev":
         await query.message.edit_text(
             text=script.DEV_TXT,
-            reply_markup=InlineKeyboardMarkup( [[               
+            reply_markup=InlineKeyboardMarkup([[
                InlineKeyboardButton("🗑️ᴄʟᴏsᴇ🗑️", callback_data = "close"),
                InlineKeyboardButton("⌫ʙᴀᴄᴋ☽", callback_data = "start")
                ]]
@@ -85,15 +92,30 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.message.delete()
 
 
-@Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
-async def send_doc(client,message):
-       media = await client.get_messages(message.chat.id,message.message_id)
-       file = media.document or media.video or media.audio 
-       filename = file.file_name
-       filesize = humanize.naturalsize(file.file_size)
-       fileid = file.file_id
-       await message.reply_text(
-       f"""__Wʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴍᴇ ᴛᴏ ᴅᴏ ᴡɪᴛʜ ᴛʜɪs ғɪʟᴇ.?__\n\n**Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`\n\n**Fɪʟᴇ Sɪᴢᴇ** :- `{filesize}`"""
-       ,reply_to_message_id = message.message_id,
-       reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("✅ ʀᴇɴᴀᴍᴇ ✅",callback_data = "rename")
-       ,InlineKeyboardButton("❌ ᴄᴀɴᴄᴇʟ ❌",callback_data = "cancel")  ]]))
+@Client.on_message(filters.command('logs') & filters.user(ADMIN))
+async def log_file(client, message):
+    try:
+        await message.reply_document('TelegramBot.log')
+    except Exception as e:
+        await message.reply_text(f"Error:\n`{e}`")
+
+@Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
+async def rename_start(client, message):
+    file = getattr(message, message.media.value)
+    filename = file.file_name
+    filesize = humanize.naturalsize(file.file_size) 
+    fileid = file.file_id
+    try:
+        text = f"""**Wʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴍᴇ ᴛᴏ ᴅᴏ ᴡɪᴛʜ ᴛʜɪs ғɪʟᴇ.?**\n\n**Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`\n\n**Fɪʟᴇ Sɪᴢᴇ** :- `{filesize}`"""
+        buttons = [[ InlineKeyboardButton("✅ ʀᴇɴᴀᴍᴇ ✅", callback_data="rename") ],
+                   [ InlineKeyboardButton("❌ ᴄᴀɴᴄᴇʟ ❌", callback_data="cancel") ]]
+        await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
+        await sleep(FLOOD)
+    except FloodWait as e:
+        await sleep(e.value)
+        text = f"""**Wʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴍᴇ ᴛᴏ ᴅᴏ ᴡɪᴛʜ ᴛʜɪs ғɪʟᴇ.?__**\n\n**Fɪʟᴇ Nᴀᴍᴇ* :- `{filename}`\n\n**Fɪʟᴇ Sɪᴢᴇ** :- `{filesize}`"""
+        buttons = [[ InlineKeyboardButton("✅ ʀᴇɴᴀᴍᴇ ✅", callback_data="rename") ],
+                   [ InlineKeyboardButton("❌ ᴄᴀɴᴄᴇʟ ❌", callback_data="cancel") ]]
+        await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
+    except:
+        pass
