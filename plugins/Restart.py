9d98f2e8474b99174d4930f7b38fefd046bcc280
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2023-01-19 18:47:35 +0530

    Update Restart.py

--- plugins/Restart.py
+++ plugins/Restart.py
@@ -1,7 +1,7 @@
 import re, os, sys
 from pyrogram import Client, filters, enums
 
-@Client.on_message(filters.command("restart") & filters.user(ADMINS))
+@Client.on_message(filters.command("restart") & filters.user(ADMIN))
 async def stop_button(bot, message):
     msg = await bot.send_message(text="**🔄 𝙿𝚁𝙾𝙲𝙴𝚂𝚂𝙴𝚂 𝚂𝚃𝙾𝙿𝙴𝙳. 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶...**", chat_id=message.chat.id)       
     await asyncio.sleep(3)
