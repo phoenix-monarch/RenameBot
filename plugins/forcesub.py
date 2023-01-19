9be15df26846d5565f9ce9532117de48220bc791
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2023-01-19 15:07:10 +0530

    Update forcesub.py

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -1,10 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from helper.progress import not_subscribed 
+from config import FORCE_SUB
 from Script import script
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def is_not_subscribed(client, message):
-    buttons = [[ InlineKeyboardButton(text="📢Join Update Channel📢", url=client.invitelink) ]]
+    buttons = [[ InlineKeyboardButton(text="📢Join Update Channel📢", url=f"https://t.me/{FORCE_SUB}") ]]
     text = script.FSUB_MSG
     await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
